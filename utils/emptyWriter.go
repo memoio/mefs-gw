c573bf3af9515a53ac1f084943cd93a7afe0d53a
xcshuan <xcshuan@hotmail.com>
2022-02-09 21:32:37 +0800

    improve some localfs code

--- utils/emptyWriter.go
+++ utils/emptyWriter.go
@@ -5,8 +5,14 @@ import "io"
 var _ io.Writer = (*EmptyWriter)(nil)
 
 type EmptyWriter struct {
+	size int64
 }
 
 func (ew *EmptyWriter) Write(p []byte) (int, error) {
+	ew.size += int64(len(p))
 	return len(p), nil
+}
+
+func (ew *EmptyWriter) Size() int64 {
+	return ew.size
 }
--- utils/localfs_test.go
+++ utils/localfs_test.go
@@ -2,8 +2,11 @@ package utils
 
 import (
 	"bytes"
+	"fmt"
 	"io"
+	"log"
 	"math/rand"
+	"strconv"
 	"testing"
 	"time"
 
@@ -38,7 +41,7 @@ func TestSimplefs(t *testing.T) {
 	io.Copy(ew, r)
 	c.Close()
 
-	sfs.FinishPut(bucketName, objectName)
+	sfs.FinishPut(bucketName, objectName, int64(len(data)), true)
 
 	cid, err := sfs.CidOf(bucketName, objectName)
 	if err != nil {
@@ -46,6 +49,62 @@ func TestSimplefs(t *testing.T) {
 	}
 
 	t.Error(cid)
+}
+
+func TestSimplefsMultiFile(t *testing.T) {
+	root := "~/gw_test"
+	root, err := homedir.Expand(root)
+	if err != nil {
+		t.Fatal(nil)
+	}
+	sfs, err := OpenLocalFS(root)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	bucketName := "testbucket"
+	sfs.MakeBucket(bucketName)
+
+	info, err := sfs.GetBucketInfo(bucketName)
+	log.Println("info:", info, "err:", err)
+
+	data := make([]byte, 20*1024*1024)
+	fillRandom(data)
+
+	for i := 0; i < 100; i++ {
+		objectName := "data" + strconv.Itoa(i)
+		buf := bytes.NewBuffer(data)
+		r, c, err := sfs.PutObject(bucketName, objectName, buf)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		ew1 := &EmptyWriter{}
+		io.Copy(ew1, r)
+		c.Close()
+
+		sfs.FinishPut(bucketName, objectName, int64(len(data)), true)
+
+		_, err = sfs.CidOf(bucketName, objectName)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		obinfo, err := sfs.GetObjectInfo(bucketName, objectName)
+		log.Println("obinfo:", obinfo, "err:", err)
+
+		rc, _, err := sfs.GetObject(bucketName, objectName, 0)
+		if err != nil {
+			t.Fatal(err)
+		}
+		ew2 := &EmptyWriter{}
+		io.Copy(ew2, rc)
+		rc.Close()
+	}
+
+	loi, err := sfs.ListObjects(bucketName)
+
+	fmt.Println("loi:", loi, "err:", err)
 }
 
 func fillRandom(p []byte) {
